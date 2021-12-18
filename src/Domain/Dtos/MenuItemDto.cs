5a6f392c377b013b271cfe685696c034fea6b46c
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-12-18 23:54:04 +0530

    fix: correct mapping

--- src/Domain/Dtos/MenuItemDto.cs
+++ src/Domain/Dtos/MenuItemDto.cs
@@ -7,7 +7,7 @@ public class MenuItemDto
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public MenuItemType MenuItemType { get; set; }
+    public MenuItemTypeDto MenuItemType { get; set; }
     public double Price { get; set; }
     public string ImageUrl { get; set; }
     public string Ingrediants { get; set; }
