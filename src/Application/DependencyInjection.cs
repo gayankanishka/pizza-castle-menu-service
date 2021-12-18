b33832476ce8fb5a35288345484326e9d6977437
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-12-18 22:32:40 +0530

    feat: add application layer

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PizzaCastle.MenuService.Application;
+
+/// <summary>
+/// Dependency injection extension to configure Application layer services.
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Configure Application layer services.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns>The service collection.</returns>
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
