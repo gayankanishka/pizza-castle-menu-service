0d75d9622730adf20959ee8045805632dcddcb2e
Gayan Kanishka Wijetunga <32380979+gayankanishka@users.noreply.github.com>
2021-12-22 06:31:42 +0530

    refactor: update seed data

--- src/Infrastructure/Persistence/Configuration/MenuCategoryConfiguration.cs
+++ src/Infrastructure/Persistence/Configuration/MenuCategoryConfiguration.cs
@@ -12,17 +12,22 @@ public class MenuCategoryConfiguration : IEntityTypeConfiguration<MenuCategory>
             new MenuCategory
             {
                 Id = Guid.Parse("6E319C7E-8E1B-41BC-8784-4BDB8608ACA2"),
-                Name = "Pizza",
+                Name = "Meat",
             },
             new MenuCategory
             {
                 Id = Guid.Parse("F3213095-31CD-4647-8FD5-C04A18A2484B"),
-                Name = "Drinks",
+                Name = "Vegetarian",
             },
             new MenuCategory
             {
                 Id = Guid.Parse("FD7D057F-C0B7-404C-ABAF-1555E474C2D5"),
-                Name = "Desserts"
+                Name = "Grill"
+            },
+            new MenuCategory
+            {
+                Id = Guid.Parse("3457A568-216D-41C1-A586-36483280A3DA"),
+                Name = "Spicy"
             });
     }
 }
--- src/Infrastructure/Persistence/Configuration/MenuItemConfiguration.cs
+++ src/Infrastructure/Persistence/Configuration/MenuItemConfiguration.cs
@@ -13,33 +13,83 @@ public class MenuItemConfiguration : IEntityTypeConfiguration<MenuItem>
             new MenuItem
             {
                 Id = Guid.NewGuid(),
-                Name = "Pizza Margherita",
+                Name = "Pilla Pizza",
                 Description = "The classic pizza",
                 Price = 1500,
-                ImageUrl =
-                    "https://static.toiimg.com/thumb/56868564.cms?imgsize=1948751&width=800&height=800",
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2021/11/pilla-pizza-2.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2021/11/pilla-pizza-2-300x300.png",
                 MenuCategoryId = Guid.Parse("6E319C7E-8E1B-41BC-8784-4BDB8608ACA2"),
                 Ingrediants = "Tomato, cheese, olive oil"
             },
             new MenuItem{
                 Id = Guid.NewGuid(),
-                Name = "Hot and Spicy Chicken Pizza",
+                Name = "Mas Kade Pizza",
                 Description = "The spicy pizza",
-                Price = 1800,
-                ImageUrl =
-                    "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg",
+                Price = 2600,
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2019/12/AP_web_photos4.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2019/12/AP_web_photos4-300x300.png",
                 MenuCategoryId = Guid.Parse("6E319C7E-8E1B-41BC-8784-4BDB8608ACA2"),
-                Ingrediants = "Chicken, cheese, olive oil"
+                Ingrediants = "Chicken, ham, mutton, chicken sausage, Chicken Bacon, salami & mozzarella."
             },
             new MenuItem{
                 Id = Guid.NewGuid(),
-                Name = "Tandoori chicken pizza",
+                Name = "Cheese Hatharak Pizza",
                 Description = "The spicy pizza",
-                Price = 2300,
-                ImageUrl =
-                    "https://i2.wp.com/www.pepperdelight.com/wp-content/uploads/2016/06/pepper-delight-tandoori-chicken-pizza-3.jpg?resize=3728%2C3696",
-                MenuCategoryId = Guid.Parse("6E319C7E-8E1B-41BC-8784-4BDB8608ACA2"),
-                Ingrediants = "Tandoori chicken, cheese, olive oil"
+                Price = 2000,
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2020/04/AP_web_photos31.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2020/04/AP_web_photos31-300x300.png",
+                MenuCategoryId = Guid.Parse("F3213095-31CD-4647-8FD5-C04A18A2484B"),
+                Ingrediants = "Swiss, parmesan, paneer & mozzarella."
+            },
+            new MenuItem{
+                Id = Guid.NewGuid(),
+                Name = "Salami Pizza",
+                Description = "The spicy pizza",
+                Price = 1900,
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2019/12/AP_web_photos2.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2019/12/AP_web_photos2-300x300.png",
+                MenuCategoryId = Guid.Parse("FD7D057F-C0B7-404C-ABAF-1555E474C2D5"),
+                Ingrediants = "Salami & mozzarella."
+            },
+            new MenuItem{
+                Id = Guid.NewGuid(),
+                Name = "Meat Kawalam Pizza",
+                Description = "The spicy pizza",
+                Price = 2450,
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2019/12/AP_web_photos3.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2019/12/AP_web_photos3-300x300.png",
+                MenuCategoryId = Guid.Parse("FD7D057F-C0B7-404C-ABAF-1555E474C2D5"),
+                Ingrediants = "Mutton, bacon, chicken & mozzarella."
+            },
+            new MenuItem{
+                Id = Guid.NewGuid(),
+                Name = "Vegetable Nai Miris Pizza",
+                Description = "The spicy pizza",
+                Price = 1650,
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2020/06/AP_web_photos33.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2020/06/AP_web_photos33-300x300.png",
+                MenuCategoryId = Guid.Parse("F3213095-31CD-4647-8FD5-C04A18A2484B"),
+                Ingrediants = "Vegetables & mozzarella."
+            },
+            new MenuItem{
+                Id = Guid.NewGuid(),
+                Name = "Chicken Hellfire Pizza",
+                Description = "The spicy pizza",
+                Price = 2500,
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2020/01/AP_web_photos32.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2020/01/AP_web_photos32-300x300.png",
+                MenuCategoryId = Guid.Parse("3457A568-216D-41C1-A586-36483280A3DA"),
+                Ingrediants = "Chicken, nai miris, onions, curry leaves & mozzarella."
+            },
+            new MenuItem{
+                Id = Guid.NewGuid(),
+                Name = "Chicken Nai Miris Pizza",
+                Description = "The spicy pizza",
+                Price = 2000,
+                ImageUrl = "https://arthurs.lk/wp-content/uploads/2020/04/AP_web_photos29-1.png",
+                ImageThumbnailUrl = "https://arthurs.lk/wp-content/uploads/2020/04/AP_web_photos29-1-300x300.png",
+                MenuCategoryId = Guid.Parse("3457A568-216D-41C1-A586-36483280A3DA"),
+                Ingrediants = "Chicken, nai miris, onions, curry leaves & mozzarella."
             });
     }
-}
+}   
